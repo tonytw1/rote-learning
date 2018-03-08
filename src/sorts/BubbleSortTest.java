259195579e40005cd430b76416210c8eb8b2e866
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-03-08 19:55:28 +0000

    Clean ups around the last element.

--- src/sorts/BubbleSortTest.java
+++ src/sorts/BubbleSortTest.java
@@ -6,31 +6,27 @@ public class BubbleSortTest {
 
     @Test
     public void casSortList() {
-        int[] input = {5, 4, 3, 5, 2, 1};
+        int[] input = {5, 4, 3, 2, 1};
 
         boolean finished = false;
-        while(finished != true) {
+        while (finished != true) {
             printArray(input);
 
             boolean swapped = false;
-            for (int i = 0; i < input.length; i++) {
+            for (int i = 0; i < input.length - 1; i++) {
                 int current = input[i];
-                if (i > 0) {
-                    int previous = input[i - 1];
-                    if (current < previous) {
-                        System.out.println("!!!! " + current + " / " + previous);
-                        input[i] = previous;
-                        input[i - 1] = current;
-                        swapped = true;
-                    }
+                int next = input[i + 1];
+                if (next < current) {
+                    System.out.println("!!!! " + current + " / " + next);
+                    input[i + 1] = current;
+                    input[i] = next;
+                    swapped = true;
                 }
             }
-
             finished = !swapped;
         }
 
         printArray(input);
-
     }
 
     public void printArray(int[] input) {
