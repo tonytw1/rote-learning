d16b21553cab91d69c3cb0317b067dce186e906e
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-06-29 13:37:00 +0100

    Moving count solution is order n with about order n! off by one edges.

--- src/leetcode/DirectorOfPhotography2.java
+++ src/leetcode/DirectorOfPhotography2.java
@@ -2,98 +2,112 @@ package leetcode;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DirectorOfPhotography2 {
 
     @Test
     public void test() {
+        assertEquals(3, getValid(".PBAAP.B", 1, 3));
+        assertEquals(0, getValid("", 1, 3));
+        assertEquals(0, getValid("BA...P", 2, 4));
+        assertEquals(0, getValid("PA...B", 2, 4));
         assertEquals(1, getValid("PAB", 1, 2));
-        assertEquals(1, getValid("P..A..BB", 2, 3));
+        assertEquals(2, getValid("P.PAAB", 1, 2));
+        assertEquals(0, getValid("P.PAAB", 2, 2));
+        assertEquals(1, getValid("PPAB", 1, 1));
+        assertEquals(1, getValid("..BBAP.", 1, 1));
+        assertEquals(1, getValid(".....P.A.B......", 1, 2));
+        assertEquals(1, getValid(".....B.A.P......", 1, 2));
+        assertEquals(2, getValid(".....B.A.P......BAP...", 1, 2));
+        assertEquals(1, getValid(".....B.A.P......BAP...", 1, 1));
+        assertEquals(1, getValid("P.A.B......", 1, 2));
+        assertEquals(1, getValid("....P.A.B", 1, 2));
+        assertEquals(0, getValid("....P.A.B", 1, 1));
+        assertEquals(1, getValid("PAB", 1, 1));
+        assertEquals(1, getValid("P...A...B", 4, 4));
+        assertEquals(2, getValid("PPAB", 1, 2));
+        assertEquals(1, getValid("P.A.B", 2, 5));
         assertEquals(0, getValid("P", 1, 2));
+        assertEquals(0, getValid("APABA", 2, 3));
+        assertEquals(1, getValid("P..A..BB", 2, 3));
+        assertEquals(0, getValid("APABA", 2, 3));
+        assertEquals(1, getValid("APABA", 1, 2));
         assertEquals(2, getValid("PABPAB", 1, 2));
         assertEquals(3, getValid("....PAAAB....", 1, 3));
         assertEquals(0, getValid("P.A.B", 1, 1));
         assertEquals(1, getValid("P.A.B", 1, 2));
         assertEquals(1, getValid("APABA", 1, 2));
-        assertEquals(0, getValid("APABA", 2, 3));
-        assertEquals(3, getValid(".PBAAP.B", 1, 3));
     }
 
     private long getValid(String C, int X, int Y) {
-        ArrayDeque<Integer> photographerPositions = new ArrayDeque<>();
-        ArrayDeque<Integer> actorPositions = new ArrayDeque<>();
-        ArrayDeque<Integer> backdropPositions = new ArrayDeque<>();
+        long totalValidShots = 0L;
+        // Intuition (which I had to look up ) is to look at the problem from the pov of the subject not the photographer!
+        // The subject only needs to know how many photographers and backdrops it can see to know how many valid photos there of it.
+        // Visiting each subject only once deals with duplicates.
 
-        long valid = 0;
+        // Walk the length of the input as the subject
+        // But we need to maintain counts of the in range photographers and backdrops; both in front and behind the subject
+        long numberOfPhotographersBehind = 0;
+        long numberOfBackdropsInFront = 0;
+        long numberOfBackdropsBehind = 0;
+        long numberOfPhotographersInFront = 0;
 
-        // Walk the input looking for photographers or backdrops which would complete a shot
-        for (int i = 0; i < C.length(); i++) {
-            char c = C.charAt(i);
-            if (c == 'P') {
-                photographerPositions.add(i);
+        // Set up 2 out riders which follow the valid range windows
+        int range = (Y - X) + 1;    // Inclusive!
 
-                // Can we find a valid artist shot behind us?
-                final int left = i - Y;
-                final int right = i - X;
-                Set<Integer> inScopeActors = actorPositions.stream().filter(a -> a >= left && a <= right).collect(Collectors.toSet());
+        int start = -(X + range) * 2;    // This intuition ensures that we read ahead far enough to prefill the in front counts; prevents in front counts going negative!
+        int r = start + X;
+        int l = start - (X + range) + 1;
 
-                for (int a : inScopeActors) {
-                    final int actorLeft = a - Y;
-                    final int actorRight = a - X;
-                    Set<Integer> validEnds = backdropPositions.stream().filter(b -> b >= actorLeft && b <= actorRight).collect(Collectors.toSet());
-                    if (!validEnds.isEmpty()) {
-                        valid += validEnds.size();
-                    }
-                }
-
-            } else if (c == 'A') {
-                // record the location of this actor
-                actorPositions.add(i);
-
-            } else if (c == 'B') {
-                backdropPositions.add(i);
-                // record the location of this backdrop
-
-                // Can we behind us?
-                // actorPositions.stream().filter(a -> a >= left && a <= right).collect(Collectors.toSet());
-
-                final int left = i - Y;
-                final int right = i - X;
-                Set<Integer> inScopeActors = actorPositions.stream().filter(a -> a >= left && a <= right).collect(Collectors.toSet());
-
-
-                for (int a : inScopeActors) {
-                    final int actorLeft = a - Y;
-                    final int actorRight = a - X;
-                    Set<Integer> validEnds = photographerPositions.stream().filter(b -> b >= actorLeft && b <= actorRight).collect(Collectors.toSet());
-                    if (!validEnds.isEmpty()) {
-                        valid += validEnds.size();
-                    }
-
+        for (int i = start; i < C.length(); i++) {
+            // Allow the out riders to move; counting items which move into range.
+            while (r < (i + X + range) - 1) {
+                r++;
+                if (r >= 0 && r < C.length()) {
+                    char cr = C.charAt(r);
+                    numberOfPhotographersInFront = cr == 'P' ? numberOfPhotographersInFront + 1 : numberOfPhotographersInFront;
+                    numberOfBackdropsInFront = cr == 'B' ? numberOfBackdropsInFront + 1 : numberOfBackdropsInFront;
                 }
             }
 
-            // Try to keep the in scope remembered positions tight for fast scanning
-            // Actors more than Y away are never going to be involved in a photo
-            // Use the right of the queue to feed items coming into range into scope
-            while (!actorPositions.isEmpty() && actorPositions.peekFirst() <= (i - Y)) {
-                actorPositions.pollFirst();
+            // Accounting for object are sliding out of range before incrementing
+            int outingGoingR = i + (X - 1);
+            if (outingGoingR >= 0 && outingGoingR < C.length()) {
+                char cr = C.charAt(outingGoingR);
+                numberOfPhotographersInFront = cr == 'P' ? numberOfPhotographersInFront - 1 : numberOfPhotographersInFront;
+                numberOfBackdropsInFront = cr == 'B' ? numberOfBackdropsInFront - 1 : numberOfBackdropsInFront;
             }
 
-            // Backdrops and photographers more than 2 Y away are never going to be involved in a photo
-            while (!photographerPositions.isEmpty() && photographerPositions.peekFirst() <=(i - (Y * 2))) {
-                photographerPositions.pollFirst();
+            while (l < (i - X)) {
+                l++;
+                if (l >= 0 && l < C.length()) {
+                    char cr = C.charAt(l);
+                    numberOfPhotographersBehind = cr == 'P' ? numberOfPhotographersBehind + 1 : numberOfPhotographersBehind;
+                    numberOfBackdropsBehind = cr == 'B' ? numberOfBackdropsBehind + 1 : numberOfBackdropsBehind;
+                }
             }
-            while (!backdropPositions.isEmpty() && backdropPositions.peekFirst() <= (i - (Y * 2))) {
-                backdropPositions.pollFirst();
+
+            // Accounting for object are sliding out of range before incrementing
+            int outingGoingL = i - (X + range);
+            if (outingGoingL >= 0 && outingGoingL < C.length()) {
+                char cr = C.charAt(outingGoingL);
+                numberOfPhotographersBehind = cr == 'P' ? numberOfPhotographersBehind - 1 : numberOfPhotographersBehind;
+                numberOfBackdropsBehind = cr == 'B' ? numberOfBackdropsBehind - 1 : numberOfBackdropsBehind;
             }
+
+            if (i >= 0) {   // Account for prereading the prefill stage
+                // If we are at a subject then evaluate the artfulness of this subject
+                final char c = C.charAt(i);
+                if (c == 'A') {
+                    // Count the number of photographers and backdrops we can see
+                    long validShots = (numberOfPhotographersBehind * numberOfBackdropsInFront) + (numberOfBackdropsBehind * numberOfPhotographersInFront);
+                    totalValidShots += validShots;
+                }
+            }
+
         }
-
-        return valid;
+        return totalValidShots;
     }
 
 }
